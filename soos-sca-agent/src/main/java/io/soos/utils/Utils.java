355bc94cb1473b7d99ff980a6da45275d8f6613b
SOOS-JAlvarez <jalvarez@soos.io>
2022-06-29 12:08:10 -0300

    PA-5418 more code that should have been removed on previous commit...

--- soos-sca-agent/src/main/java/io/soos/utils/Utils.java
+++ soos-sca-agent/src/main/java/io/soos/utils/Utils.java
@@ -12,62 +12,6 @@ public class Utils {
 
     private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-    public static void addContentPolicyToShowSoosImgs(TeamcityContext teamcityContext) {
-        String pathSeparator = PluginConstants.SLASH;
-        String teamcityDataPath = teamcityContext.getDataPath();
-        if ( OSValidator.isWindows() ){
-            pathSeparator = PluginConstants.BACK_SLASH;
-            teamcityDataPath = teamcityDataPath.replace(PluginConstants.C_BACK_SLASH_COLON_DOUBLE_BACK_SLASH, PluginConstants.C_COLON_BACK_SLASH);
-        }
-        StringBuilder internalPropertiesFilePath = new StringBuilder();
-        internalPropertiesFilePath.append(teamcityDataPath);
-        internalPropertiesFilePath.append(pathSeparator);
-        internalPropertiesFilePath.append(PluginConstants.CONFIG);
-        internalPropertiesFilePath.append(pathSeparator);
-        internalPropertiesFilePath.append(pathSeparator);
-        internalPropertiesFilePath.append(PluginConstants.INTERNAL_PROPERTIES_FILE);
-        File internalPropertiesFile = new File(internalPropertiesFilePath.toString());
-        try {
-            internalPropertiesFile.createNewFile();
-            boolean lineEdited = false;
-            StringBuffer stringBuffer = new StringBuffer();
-            Scanner reader = new Scanner(internalPropertiesFile);
-            while (reader.hasNextLine()) {
-                String line = reader.nextLine();
-                String firstChar = !line.isBlank() ? Character.toString(line.trim().charAt(0)) : "";
-                String[] contantsConditions = { PluginConstants.CONTENT_POLICY_PROPERTY, PluginConstants.IMG_SRC_POLICY, PluginConstants.SOOS_IMAGES_PRODUCTION_CDN };
-                if ( !firstChar.equals("#") &&  Arrays.stream(contantsConditions).allMatch(line::contains) ) {
-                    reader.close();
-                    return;
-                } else if ( !firstChar.equals("#") && line.contains(PluginConstants.CONTENT_POLICY_PROPERTY)
-                        && line.contains(PluginConstants.IMG_SRC_POLICY) ) {
-                    int index = line.indexOf("blob: ");
-                    line = line.substring(0, index + 6).concat(PluginConstants.SOOS_IMAGES_PRODUCTION_CDN).concat(PluginConstants.BLANK_SPACE).concat(line.substring(index + 6));
-                    lineEdited = true;
-                }
-                stringBuffer.append(line.concat(System.lineSeparator()));
-            }
-            reader.close();
-            if ( !lineEdited ) {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(internalPropertiesFile,true));
-                StringBuilder fileText = new StringBuilder();
-                fileText.append(PluginConstants.CONTENT_POLICY_PROPERTY);
-                fileText.append(PluginConstants.EQUAL);
-                fileText.append(PluginConstants.IMG_SRC_POLICY);
-                fileText.append(PluginConstants.SOOS_IMAGES_PRODUCTION_CDN);
-                writer.newLine();
-                writer.write(fileText.toString());
-                writer.close();
-                return;
-            }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(internalPropertiesFile));
-            writer.write(stringBuffer.toString());
-            writer.close();
-        } catch (IOException e) {
-            LOG.severe("Failed to write to internal.properties file: ".concat(e.getMessage()));
-        }
-    }
-
     public static String getTeamcityDataPath(String teamcityServerPath) {
         String pathSeparator = PluginConstants.SLASH;
         if ( OSValidator.isWindows() ){
